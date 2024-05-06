8244c431e87f0c97b42f4bfd29d8b42ccc84d365
swooncreative <73920559+swooncreative@users.noreply.github.com>
2024-05-06 12:41:04 -0700

    test using different port

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   server: {
-    port: process.env.PORT || 3000, // default: 3000
+    // port: process.env.PORT || 3000, // default: 3000
+    port: 8000, // default: 3000
   },
 
   serverMiddleware: [
